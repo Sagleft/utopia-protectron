c75075cb1380be4e31f3d59137832047b0ae546f
Sagleft <sagleft@inbox.ru>
2023-02-23 12:42:00 +0200

    add chatbot base

--- go.mod
+++ go.mod
@@ -2,18 +2,24 @@ module bot
 
 go 1.18
 
-require github.com/stretchr/testify v1.8.1
+require (
+	github.com/Sagleft/uchatbot-engine v1.6.0
+	github.com/Sagleft/utopialib-go/v2 v2.4.0
+	github.com/fatih/color v1.14.1
+	github.com/stretchr/testify v1.8.1
+)
 
 require (
 	github.com/Sagleft/swiss-knife v1.0.8 // indirect
-	github.com/Sagleft/uchatbot-engine v1.6.0 // indirect
-	github.com/Sagleft/utopialib-go/v2 v2.4.0 // indirect
 	github.com/beefsack/go-rate v0.0.0-20220214233405-116f4ca011a0 // indirect
 	github.com/ctengiz/evtwebsocket v0.0.0-20180717104640-fc3583982591 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagleft/simple-cron v1.5.0 // indirect
 	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/grignaak/tribool.v1 v1.0.0-20150312065122-d6bb19d816df // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/bot/utopia.go
+++ pkg/bot/utopia.go
@@ -1,23 +1,61 @@
 package bot
 
 import (
-	"errors"
-
+	"github.com/Sagleft/uchatbot-engine"
 	utopiago "github.com/Sagleft/utopialib-go/v2"
+	"github.com/Sagleft/utopialib-go/v2/pkg/structs"
+	"github.com/fatih/color"
 )
 
 type uBot struct {
-	conn utopiago.Client
+	handler *uchatbot.ChatBot
 }
 
-func NewUtopiaBot(cfg utopiago.Config) (Bot, error) {
-	b := &uBot{
-		conn: utopiago.NewUtopiaClient(cfg),
-	}
+type uBotConfig struct {
+	WelcomeMessage string          `json:"welcomeMessage"`
+	UtopiaConfig   utopiago.Config `json:"utopia"`
+}
 
-	if !b.conn.CheckClientConnection() {
-		return nil, errors.New("failed to connect to Utopia client")
+func NewUtopiaBot(cfg uBotConfig) (Bot, error) {
+	b := &uBot{}
+
+	var err error
+	b.handler, err = uchatbot.NewChatBot(uchatbot.ChatBotData{
+		Config: cfg.UtopiaConfig,
+		Chats: []uchatbot.Chat{
+			{ID: "TODO"}, // TODO: load from func args
+		},
+		Callbacks: uchatbot.ChatBotCallbacks{
+			OnContactMessage:        b.onContactMessage,
+			OnChannelMessage:        b.onChannelMessage,
+			OnPrivateChannelMessage: b.onPrivateChannelMessage,
+
+			WelcomeMessage: func(userPubkey string) string {
+				return cfg.WelcomeMessage
+			},
+		},
+		UseErrorCallback: true,
+		ErrorCallback:    b.onError,
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	return b, nil
+}
+
+func (b *uBot) onError(err error) {
+	color.Red(err.Error())
+}
+
+func (b *uBot) onContactMessage(message structs.InstantMessage) {
+	// TODO
+}
+
+func (b *uBot) onChannelMessage(message structs.WsChannelMessage) {
+	// TODO
+}
+
+func (b *uBot) onPrivateChannelMessage(message structs.WsChannelMessage) {
+	// TODO
 }
